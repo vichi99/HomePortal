84340ce49f5d384a0eb337c59b4c714e849263f3
Jan Vícha <john.vicha@seznam.cz>
2021-10-24 14:03:58 +0200

    update monika bot by set timer off

--- monika_bot/main.py
+++ monika_bot/main.py
@@ -26,8 +26,11 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 LIMIT_MESSAGE_SECOND = int(os.getenv("LIMIT_MESSAGE_SECOND"))
 TOPIC_LED_SET = os.getenv("TOPIC_LED_SET")
 TOPIC_LED_STATUS = os.getenv("TOPIC_LED_STATUS")
+TOPIC_LED_TIMER_OFF = os.getenv("TOPIC_LED_TIMER_OFF")
+TOPIC_LED_TIMER_OFF_STATUS = os.getenv("TOPIC_LED_TIMER_OFF_STATUS")
 
 income_led_status = 0
+income_led_timer_off_status = ""
 actual_chat_id = 0
 
 LOGGER.info("Starting monika bot")
@@ -35,7 +38,7 @@ LOGGER.info("Starting monika bot")
 # make telegram bot
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 keyboard = telebot.types.ReplyKeyboardMarkup(True)
-keyboard.row("Off")
+keyboard.row("Off", "Status")
 keyboard.row("1%", "5%", "50%", "100%")
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -43,13 +46,14 @@ def on_connect(client, userdata, flags, rc):
     LOGGER.info(f"Connected to mqtt broker. Code: {rc}")
 
     client.subscribe(TOPIC_LED_STATUS)
+    client.subscribe(TOPIC_LED_TIMER_OFF_STATUS)
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
+    global income_led_status, income_led_timer_off_status, actual_chat_id
     if msg.topic == TOPIC_LED_STATUS:
         try:
-            global income_led_status, actual_chat_id
             income_led_status = int(msg.payload)
             LOGGER.info(f"Income led status: {income_led_status}")
             if actual_chat_id:
@@ -60,10 +64,30 @@ def on_message(client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
                     actual_chat_id = 0
                 except telebot.apihelper.ApiTelegramException:
                     LOGGER.error(
-                        f"Send message to telegram failed.",
+                        f"Send  led value message to telegram failed.",
                         f" Chat ID: {actual_chat_id}",
                         exc_info=True,
                     )
+        except ValueError:
+            LOGGER.error(f"Value error '{msg.payload}' for topic: {TOPIC_LED_STATUS}")
+    elif msg.topic == TOPIC_LED_TIMER_OFF_STATUS:
+        try:
+            if check_time_format(msg.payload.decode("utf-8")):
+                income_led_timer_off_status = msg.payload.decode("utf-8")
+                LOGGER.info(f"Income timer of status: {income_led_timer_off_status}")
+                if actual_chat_id:
+                    try:
+                        bot.send_message(
+                            actual_chat_id,
+                            f"Successfully timer set to {income_led_timer_off_status}.",
+                        )
+                        actual_chat_id = 0
+                    except telebot.apihelper.ApiTelegramException:
+                        LOGGER.error(
+                            f"Send timer message to telegram failed.",
+                            f" Chat ID: {actual_chat_id}",
+                            exc_info=True,
+                        )
         except ValueError:
             LOGGER.error(f"Value error '{msg.payload}' for topic: {TOPIC_LED_STATUS}")
 
@@ -131,21 +155,34 @@ def set_led_percent(value: int):
     LOGGER.info(f"Published led percent value to : {value}%.")
 
 
-def check_already_set_value(value: int):
+def check_already_set_led_value(value: int):
     """
     If sending value and incomed value is the same send message to telegram chat.
 
     :param int value: Sending percent value for set led.
     """
-    if income_led_status == value:
+    if actual_chat_id and income_led_status == value:
         bot.send_message(
             actual_chat_id, f"Led value is already set to {income_led_status}%"
         )
 
 
+def check_already_set_timer_off(value: str):
+    """
+    If sending value and incomed value is the same send message to telegram chat.
+
+    :param str value: Sending timer off value for set led.
+    """
+    if actual_chat_id and income_led_timer_off_status == value:
+        bot.send_message(
+            actual_chat_id,
+            f"Timer off value is already set to {income_led_timer_off_status}%",
+        )
+
+
 def handling_message_for_led(message: telebot.types.Message):
     """
-    Handle message for setting led on the Monika bed.
+    Handle message for setting's led on the Monika bed.
 
     :param message: Income message from telegram.
     :type message: telebot.types.Message
@@ -155,23 +192,66 @@ def handling_message_for_led(message: telebot.types.Message):
     if message.text.lower() == "off":
         actual_chat_id = message.chat.id
         set_led_percent(0)
-        check_already_set_value(0)
+        check_already_set_led_value(0)
     elif message.text == "1%":
         actual_chat_id = message.chat.id
         set_led_percent(1)
-        check_already_set_value(1)
+        check_already_set_led_value(1)
     elif message.text == "5%":
         actual_chat_id = message.chat.id
         set_led_percent(5)
-        check_already_set_value(5)
+        check_already_set_led_value(5)
     elif message.text == "50%":
         actual_chat_id = message.chat.id
         set_led_percent(50)
-        check_already_set_value(50)
+        check_already_set_led_value(50)
     elif message.text == "100%":
         actual_chat_id = message.chat.id
         set_led_percent(100)
-        check_already_set_value(100)
+        check_already_set_led_value(100)
+    elif message.text == "Status":
+        msg = (
+            f"Led: {income_led_status}% ; "
+            + f"Timer off: {income_led_timer_off_status}"
+            + f"\nPro nastavení vypnutí napíš např. 23:05"
+        )
+
+        bot.send_message(message.chat.id, msg)
+
+
+def check_time_format(value: str) -> bool:
+    """
+    Control income time format for set timer switching off led.
+
+    :param str value: String time `5:06`, `23:59`.
+    :return: Return True if format is valid
+    :rtype: bool
+    """
+    try:
+        hours, minutes = value.split(":")
+        assert 1 <= len(hours) <= 2
+        assert len(minutes) == 2
+        assert 0 <= int(hours) <= 23
+        assert 0 <= int(minutes) <= 59
+        return True
+    except (TypeError, ValueError, AssertionError, AttributeError):
+        LOGGER.warning(f"Bad income time format: {value}")
+        return False
+
+
+def handling_message_set_timer_off(message: telebot.types.Message):
+    """
+    Handle message for setting timer off on the Monika bed.
+
+    :param message: Income message from telegram.
+    :type message: telebot.types.Message
+    """
+    if check_time_format(message.text):
+        global actual_chat_id
+        actual_chat_id = message.chat.id
+        mqtt_client.publish(TOPIC_LED_TIMER_OFF, str(message.text), qos=1, retain=True)
+        LOGGER.info(f"Published timer off value to: {message.text}.")
+        check_already_set_timer_off(message.text)
 
 
 @bot.message_handler(commands=["start"])
@@ -190,6 +270,7 @@ def send_text(message: telebot.types.Message):
     if not is_auth_user(message) or not is_message_old(message):
         return
     handling_message_for_led(message)
+    handling_message_set_timer_off(message)
 
 
 bot.polling()
